0989ab3c68c69b2ec451b36e08cbb7084803b4c3
Henrik N <henrik@nyh.se>
2012-08-10 21:57:33 +0200

    Also proxy URLs.

---
+++ TODO.md
@@ -0,0 +1,7 @@
+- memcache?
+- follow search redirects
+- change logo
+
+- links to some key pages like:
+  - http://t2b.dev/sv/Sa-funkar-riksdagen/Batmannen/
+  - http://t2b.dev/sv/Sa-funkar-riksdagen/Ordbok/batman/
--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require "rubygems"
 require "bundler"
 Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 
-require "open-uri"
+require "net/http"
 
 
 get "/" do
-  proxy_get "/Dokument-Lagar/Kammaren/Protokoll/"
+  proxy_get("/Dokument-Lagar/Kammaren/Protokoll/")
 end
 
 get "*" do
@@ -19,16 +19,33 @@ end
 
 
 def make_uri(path)
-  URI.parse("http://www.riksdagen.se" + path)
+  URI.parse("http://www.riksdagen.se" + debatmanize(path))
 end
 
 def proxy_get(path)
-  # open-uri, follows redirects etc cleverly.
-  make_uri(path).read
+  uri = make_uri(path)
+  response = Net::HTTP.get_response(uri)
+
+  content_type response['content-type']
+  batmanize response.body
 end
 
 def proxy_post(path, params)
   uri = make_uri(path)
   response = Net::HTTP.post_form(uri, params)
-  response.body
+
+  content_type response['content-type']
+  batmanize response.body
+end
+
+def batmanize(text)
+  text.gsub(/\b(t)(a)(l)(man)/i) {
+    "#{$1.tr 'Tt', 'Bb'}#$2#{$3.tr 'Ll', 'Tt'}#$4"
+  }
+end
+
+def debatmanize(text)
+  text.gsub(/\b(b)(a)(t)(man)/i) {
+    "#{$1.tr 'Bb', 'Tt'}#$2#{$3.tr 'Tt', 'Ll'}#$4"
+  }
 end
